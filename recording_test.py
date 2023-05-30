bd6e673cde494d75b4092b1a441daf1d2ed81962
rheaton64 <ryguyheaton@gmail.com>
2023-05-29 21:01:11 -0400

    updated test

--- recording_test.py
+++ recording_test.py
@@ -9,6 +9,14 @@ RECORD_SECONDS = 5
 WAVE_OUTPUT_FILENAME = "output.wav"
 
 p = pyaudio.PyAudio()
+for i in range(p.get_device_count()):
+    dev_info = p.get_device_info_by_index(i)
+    if dev_info['maxInputChannels'] > 0:
+        print(f"Input Device ID {i}: {dev_info['name']}")
+
+p.terminate()
+
+p = pyaudio.PyAudio()
 
 stream = p.open(format=FORMAT,
                 channels=CHANNELS,
